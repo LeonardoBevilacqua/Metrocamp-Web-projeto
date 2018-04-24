f9f64511dab563558cec13dc36521c79b1ac6e57
Leonardo Almeida Bevilacqua <leonardo_bevilacqua@hotmail.com>
2018-04-23 21:55:16 -0300

    form validation

---
+++ Template/scripts/script.js
@@ -0,0 +1,10 @@
+/* Jquery Form validator */
+
+$.validate({
+    lang: 'pt',
+    modules: 'security',
+    errorElementClass : 'uk-form-danger',
+    borderColorOnError : '',
+    errorMessageClass: 'uk-text-danger',
+    
+});
