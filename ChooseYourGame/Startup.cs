529f855eabef948ed73c60d8867f784a4bbb9568
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2018-05-29 11:35:21 -0300

    Test release

--- ChooseYourGame/Startup.cs
+++ ChooseYourGame/Startup.cs
@@ -28,8 +28,8 @@ namespace ChooseYourGame
         {
             services.AddMvc();
             services.AddDbContext<ChooseYourGameContext>(options =>
-                //options.UseInMemoryDatabase("DefaultConnection")
-                options.UseMySql(Configuration.GetConnectionString("DefaultConnection"))
+                options.UseInMemoryDatabase("DefaultConnection")
+                //options.UseMySql(Configuration.GetConnectionString("DefaultConnection"))
             );
             services.AddIdentity<IdentityUser, IdentityRole>()
             .AddEntityFrameworkStores<ChooseYourGameContext>()
